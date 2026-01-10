b5393e4287138401c3c19685dd3ceafb44700279
pipboy3003 <rndsmqjndr@privaterelay.appleid.com>
2026-01-10 11:58:34 +0100

    Back to 2am

--- game/game_char_logic.js
+++ game/game_char_logic.js
@@ -80,7 +80,7 @@ Object.assign(Game, {
             enemy.loot = Math.floor(enemy.loot * (1 + (fortuneLvl * 0.1)));
         }
 
-        const isLegendary = Math.random() < 0.15; 
+        const isLegendary = Math.random() < 0.05; 
         if(isLegendary) { 
             enemy.isLegendary = true; 
             enemy.name = "Legendäre " + enemy.name; 
